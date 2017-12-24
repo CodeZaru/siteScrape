33750f4279eb3e6405e0064e6a984c659a18f764
CodeZaru <teamsullivank@gmail.com>
2017-12-24 00:57:25 -0500

    x

--- services/scraper.js
+++ services/scraper.js
@@ -13,24 +13,24 @@ module.exports = callback => {
         $("div.channel-image").each((i, element) => {
 
 
-            let img = $(element).children('img').children('a').children('picture').children('source').attr('srcset');
+            let img = $(element).children('div.img').children('a').children('picture').children('source').attr('srcset');
 
-            let title = $(element).children('.channel-content').children('.article-title').children('a').text().trim();
+            let title = $(element).children('div.channel-content').children('div.article-title').children('a').text().trim();
 
-            let author = $(element).children('.channel-content').children('.byline-container').children('a').text();
+            let author = $(element).children('div.channel-content').children('div.byline-container').children('span.field-author').children('a').text();
 
-            let date = $(element).children('.channel-content').children('date').text();
+            //let date = $(element).children('div.channel-content').children('div.byline-container').children('.date').text();
 
-            let link = $(element).children('.channel-content').children('.article-title').children('a').attr('href');
+            let link = $(element).children('div.channel-content').children('.article-title').children('a').attr('href');
 
-            let description = $(element).children('.channel-content').children('.field-deck').text().trim();
+            let description = $(element).children('div.channel-content').children('.field-dek').text().trim();
 
-            articleObj = { title, img, author, date, link, description };
+            articleObj = { title, img, author, link, description };
 
             console.log(`TITLE: ${title}`);
             console.log(`IMG: ${img}`);
             console.log(`AUTHOR: ${author}`);
-            console.log(`DATE: ${date}`);
+            //console.log(`DATE: ${date}`);
             console.log(`LINK: ${link}`);    
             console.log(`DESCRIPTION: ${description}`);
             
